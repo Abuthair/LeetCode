8bec899984f8c6243d0d4e774d9d06e55841a48a
Abuthair <Sheikabuthair.pc@gmail.com>
2023-03-07 22:24:44 +0530

    Day1

---
+++ src/Coding/TwoSum.java
@@ -0,0 +1,19 @@
+package Coding;
+
+/**
+ * @author abuthair on 07/03/23.
+ * 10:19 pm
+ * @project LeetCode
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+}
