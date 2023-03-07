34807c3fcef79aaafaa6279def0de234bec94b40
Abuthair <Sheikabuthair.pc@gmail.com>
2023-03-07 22:27:36 +0530

    Day1

--- src/Coding/TwoSum.java
+++ src/Coding/TwoSum.java
@@ -6,6 +6,7 @@ package Coding;
  * @project LeetCode
  */
 public class TwoSum {
+    //Hell0
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
